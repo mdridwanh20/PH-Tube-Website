397a490fabac67cb56353b487fd0e44c9e6ec022
Ridwan <webridwan20@gmail.com>
2024-10-03 13:05:38 +0600

    problem fix

---
+++ scripts/test_code.js
@@ -0,0 +1,10 @@
+
+
+// const getTimeString = (time) => {
+//     const hours = parseInt(time / 3600)
+//     let remainingSecond = time % 3600;
+//     const minute = parseInt(remainingSecond / 60);
+//     return `${hours} hours ${minute} minute age`;
+// }
+
+// console.log(getTimeString(5454));
--- scripts/video.js
+++ scripts/video.js
@@ -17,29 +17,38 @@ const loadVideo = () => {
     .catch((Error) => console.log('this is error', Error))
 }
 
-// card demo:
-const cardDemo = {
-    "category_id": "1003",
-    "video_id": "aaac",
-    "thumbnail": "https://i.ibb.co/NTncwqH/luahg-at-pain.jpg",
-    "title": "Laugh at My Pain",
-    "authors": [
-        {
-            "profile_picture": "https://i.ibb.co/XVHM7NP/kevin.jpg",
-            "profile_name": "Kevin Hart",
-            "verified": true,
-        }
-    ],
 
-    "others": {
-        "views": "1.1K",
-        "posted_date": "13885"
-    },
-    "description": "Comedian Kevin Hart brings his unique brand of humor to life in 'Laugh at My Pain.' With 1.1K views, this show offers a hilarious and candid look into Kevin's personal stories, struggles, and triumphs. It's a laugh-out-loud experience filled with sharp wit, clever insights, and a relatable charm that keeps audiences coming back for more."
-};
+// tim setup function;
+    const getTimeString = (time) => {
+        const hours = parseInt(time / 3600)
+        let remainingSecond = time % 3600;
+        const minute = parseInt(remainingSecond / 60);
+        return `${hours} hr ${minute} min`;
+    }
 
 
 
+
+// const demoVideo = {
+//     "category_id": "1003",
+//     "video_id": "aaae",
+//     "thumbnail": "https://i.ibb.co/Yc4p5gD/inside-amy.jpg",
+//     "title": "Inside Amy Schumer",
+//     "authors": [
+//         {
+//             "profile_picture": "https://i.ibb.co/YD2mqH7/amy.jpg",
+//             "profile_name": "Amy Schumer",
+//             "verified": ""
+//         }
+//     ],
+//     "others": {
+//         "views": "3.6K",
+//         "posted_date": "15147"
+//     },
+//     "description": "'Inside Amy Schumer' is a comedy show by the popular comedian Amy Schumer, blending sharp satire and unfiltered humor to tackle everyday issues and societal norms. With 3.6K views, the show promises a blend of hilarious sketches, thought-provoking stand-up, and candid interviews. It's a must-watch for fans of bold, edgy comedy."
+// }
+
+                        // problem 55 line kvabe aslo.
 // video display:
 const videoDisplay = (videos) => {
     const videosContainer = document.getElementById('videosContainer');
@@ -48,11 +57,18 @@ const videoDisplay = (videos) => {
         const card = document.createElement('div');
         card.classList = 'card card-compact'
         card.innerHTML = `
-        <figure class="h-[200px]">
+        <figure class="h-[200px] relative">
             <img
             src="${video.thumbnail}"
             class="h-full w-full object-cover"
-            alt="Shoes" />
+            alt="Shoes"/>
+            ${
+                video.others.posted_date?.length == 0 
+                ? ""
+                : `<span class="absolute right-2 bottom-2 bg-black px-2 text-white">
+                ${getTimeString(video.others.posted_date)}</span>`
+            }
+            
         </figure>
 
          <div class="flex gap-5 py-4">
@@ -60,32 +76,26 @@ const videoDisplay = (videos) => {
             <div>
                 <img class="w-10 h-10 rounded-full object-cover" src="${video.authors[0].profile_picture}" alt="">
             </div>
-
+            
             <div>
                 <h2 class="font-bold">${video.title}</h2>
 
                 <div class="flex items-center gap-2">
                     <p>${video.authors[0].profile_name}</p>
-                    <img class="w-5 h-5 " src="https://img.icons8.com/?size=100&id=D9RtvkuOe31p&format=png&color=000000">
+
+                    ${video.authors[0].verified === true
+                        ? `<img class="w-5 h-5 " src="https://img.icons8.com/?size=100&id=D9RtvkuOe31p&format=png&color=000000">` : ''}
                 </div>
                 
                 <p>${video.others.views}</p>
             </div>
             
         </div>
-
-
-
-
-
-
-
-        `;
+    `;
 
         videosContainer.append(card)
     })
 }
-
 
 
 // create display categories:
