63995e70f6e0d0fb1882dbfbc022be7386a69d9d
Ridwan <webridwan20@gmail.com>
2024-10-04 08:29:57 +0600

    some problem fix

---
+++ scripts/card_demo.js
@@ -0,0 +1,20 @@
+
+
+// const demoVideo = {
+//     "category_id": "1003",
+//     "video_id": "aaae",
+//     "thumbnail": "https://i.ibb.co/Yc4p5gD/inside-amy.jpg",
+//     "title": "Inside Amy Schumer",
+//     "authors": [
+//         {
+//             "profile_picture": "https://i.ibb.co/YD2mqH7/amy.jpg",
+//             "profile_name": "Amy Schumer",
+//             "verified": ""
+//         }
+//     ],
+//     "others": {
+//         "views": "3.6K",
+//         "posted_date": "15147"
+//     },
+//     "description": "'Inside Amy Schumer' is a comedy show by the popular comedian Amy Schumer, blending sharp satire and unfiltered humor to tackle everyday issues and societal norms. With 3.6K views, the show promises a blend of hilarious sketches, thought-provoking stand-up, and candid interviews. It's a must-watch for fans of bold, edgy comedy."
+// }
--- scripts/video.js
+++ scripts/video.js
@@ -1,66 +1,99 @@
 // 1. fetch: load and show categories on html
 
-// create loadCategories:
-    const loadCategories = () => {
-    // fetch the data:
-    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
-        .then((res) => res.json())
-        .then((data) => displayCategories(data.categories))
-        .catch((error) => console.log(error));
-    }
 
+// create loadCategories:
+const loadCategories = () => {
+// fetch the data:
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories))
+    .catch((error) => console.log(error));
+}
+    
 //  video categories
-const loadVideo = () => {
-    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+const loadVideo = (searchTex = "") => {
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchTex}`)
+
     .then((res) => res.json())
     .then((data) => videoDisplay(data.videos))
     .catch((Error) => console.log('this is error', Error))
 }
 
+// remove active class
+const removeActiveClass = () => {
+    const buttons = document.getElementsByClassName('category-btn')
+    for (let btn of buttons){
+        btn.classList.remove("active")
+    }
+    
+}
 
 // load category video here:
 const loadCategoriesVideo = (id) => {
     // alert(id)
-
     fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
     .then((res) => res.json())
-    .then((data) => videoDisplay(data.category))
+    
+    .then((data) => {
+        removeActiveClass()
+        const activeBtn = document.getElementById(`btn-${id}`);
+        activeBtn.classList.add('active')
+        
+        videoDisplay(data.category);
+    })
+
     .catch((Error) => console.log('this is error', Error))
 }
 
+// load data button details:
+const loadDetails = async(videoId) => {
+    console.log('this is detail ', videoId);
+    const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`
+    const res = await fetch(url)
+    const data = await res.json()
+    displayDetails(data.video)
+}
 
-// const demoVideo = {
-//     "category_id": "1003",
-//     "video_id": "aaae",
-//     "thumbnail": "https://i.ibb.co/Yc4p5gD/inside-amy.jpg",
-//     "title": "Inside Amy Schumer",
-//     "authors": [
-//         {
-//             "profile_picture": "https://i.ibb.co/YD2mqH7/amy.jpg",
-//             "profile_name": "Amy Schumer",
-//             "verified": ""
-//         }
-//     ],
-//     "others": {
-//         "views": "3.6K",
-//         "posted_date": "15147"
-//     },
-//     "description": "'Inside Amy Schumer' is a comedy show by the popular comedian Amy Schumer, blending sharp satire and unfiltered humor to tackle everyday issues and societal norms. With 3.6K views, the show promises a blend of hilarious sketches, thought-provoking stand-up, and candid interviews. It's a must-watch for fans of bold, edgy comedy."
-// }
+// display details video:
+const displayDetails = (video) => {
+
+    // show modal here
+    const detailContainer = document.getElementById('modelContent');
+    document.getElementById('videoModal').showModal();
+
+    // show modal innerHtml cnt;
+    detailContainer.innerHTML = `
+        <img src="${video.thumbnail}">
+        <p class="mt-4">${video.description}</p>
+    `;
+}
 
 
-
-
-            // problem 55 line kvabe aslo.
+        // problem 55 line kvabe aslo.
 // video display:
 const videoDisplay = (videos) => {
     const videosContainer = document.getElementById('videosContainer');
     videosContainer.innerHTML = "";
+    // drawing work 
+    if (videos.length === 0){
+        videosContainer.classList.remove("grid")
+        videosContainer.innerHTML = `
+        
+        <div class="w-full h-[300px] flex justify-center items-center flex-col gap-6">
+            <img src="./Icon.png" alt="">
+            <h2 class="text-center font-bold text-xl">No content here in this category</h2>
+        </div>
+        `
+    } else {
+        videosContainer.classList.add("grid")
+    }
 
     videos.forEach((video) => {
-        // console.log(video);
+        console.log(video);
+
+        // video card create here
         const card = document.createElement('div');
-        card.classList = 'card card-compact'
+        card.classList = 'card border p-4 card-compact'
         
         card.innerHTML = `
         <figure class="h-[200px] relative">
@@ -82,21 +115,22 @@ const videoDisplay = (videos) => {
             <div>
                 <img class="w-10 h-10 rounded-full object-cover" src="${video.authors[0].profile_picture}" alt="">
             </div>
-            
             <div>
                 <h2 class="font-bold">${video.title}</h2>
-
                 <div class="flex items-center gap-2">
                     <p>${video.authors[0].profile_name}</p>
 
                     ${video.authors[0].verified === true
                         ? `<img class="w-5 h-5 " src="https://img.icons8.com/?size=100&id=D9RtvkuOe31p&format=png&color=000000">` : ''}
                 </div>
-                
                 <p>${video.others.views}</p>
             </div>
-            
         </div>
+
+            <div class="text-right">
+                <p><button onclick="loadDetails('${video.video_id}')" class="btn btn-sm btn-primary rounded-sm text-white">Details</button></p>
+            </div>
+
     `;
      videosContainer.append(card)
 
@@ -107,13 +141,13 @@ const videoDisplay = (videos) => {
 // create display categories:
     const displayCategories = (categories) => {
         const categoriesContainer = document.getElementById('categories');
-        
+
         categories.forEach((item) => {
             // console.log(item);
             const buttonContainer = document.createElement('div')
             buttonContainer.innerHTML = `
 
-            <button onclick="loadCategoriesVideo(${item.category_id})" class="btn">
+            <button id="btn-${item.category_id}" onclick="loadCategoriesVideo(${item.category_id})" class="btn category-btn">
                 ${item.category}
             </button>
             `
@@ -122,7 +156,13 @@ const videoDisplay = (videos) => {
     };
 
 
+// search input here
+    document.getElementById('searchInput').addEventListener('keyup', (e) => {
+        loadVideo(e.target.value);
+    })
+
 
 loadCategories();
 loadVideo()
+
 
